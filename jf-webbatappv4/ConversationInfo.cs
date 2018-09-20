ab58fa51f742a84f09bfbda26a76aa83f02ac05e
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-09-20 14:46:22 -0700

    Add a bot

---
+++ jf-webbatappv4/ConversationInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_EchoBot_With_State
+{
+    public class ConversationInfo
+    {
+        public int TurnCount { get; set; } = 0;
+
+        public bool IsNameExpected { get; set; } = false;
+
+        public string Name { get; set; } = null;
+    }
+}
