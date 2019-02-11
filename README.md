91bab26cb8691215d32a8be862075517288a3b7b
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2019-02-11 10:11:59 -0800

    clean plate

--- README.md
+++
@@ -1,2 +0,0 @@
-# BotScratch
-For testing bots and the bot service. This repo is transitory.
--- jf-webbatappv4/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild AspNetCore-EchoBot-With-State.sln -p:DeployOnBuild=true -p:PublishProfile=jf-webbatappv4-Web-Deploy.pubxml -p:Password=6gxj46qywbuiwtuHY7tngfE6Qiq6mqbbcJnh2KxM3K8if41LYbJXbBePRdyB
-
