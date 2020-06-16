9f12e5f8ae06c6e8a6f40c63a386f8ee3ef1b076
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2020-06-16 09:08:44 -0700

    wip

---
+++ 81.skills-skilldialog/DialogRootBot/Bots/RootBot.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Microsoft.BotBuilderSamples.DialogRootBot.Bots
+{
+    public class RootBot<T> : ActivityHandler
+        where T : Dialog
+    {
+        private Dialog MainDialog { get; }
+        private DialogManager DialogManager { get; }
+
+        public RootBot(T mainDialog)
+        {
+            MainDialog = mainDialog;
+            DialogManager = new DialogManager(MainDialog);
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            await DialogManager.OnTurnAsync(turnContext, cancellationToken);
+        }
+
+        // Load attachment from embedded resource.
+        private Attachment CreateAdaptiveCardAttachment()
+        {
+            var cardResourcePath = "Microsoft.BotBuilderSamples.DialogRootBot.Cards.welcomeCard.json";
+
+            using var stream = GetType().Assembly.GetManifestResourceStream(cardResourcePath);
+            using var reader = new StreamReader(stream);
+            var adaptiveCard = reader.ReadToEnd();
+            return new Attachment
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCard)
+            };
+        }
+    }
+}
---
+++ 81.skills-skilldialog/DialogRootBot/Dialogs/AlternateDialog.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
+using System.Collections.Generic;
+
+namespace Microsoft.BotBuilderSamples.DialogRootBot.Dialogs
+{
+    public class AlternateDialog : AdaptiveDialog
+    {
+        public AlternateDialog() : base("AdaptiveMainDialog")
+        {
+
+            base.Triggers = new List<OnCondition>
+            {
+                new OnConversationUpdateActivity()
+                {
+                    Actions = WelcomeUsersSteps
+                },
+                new OnUnknownIntent()
+                {
+                    Actions= AccessASkillSteps
+                }
+            };
+        }
+
+        private static List<Dialog> WelcomeUsersSteps { get; } =
+            new List<Dialog>
+            {
+                new Foreach()
+                {
+                    ItemsProperty = "turn.activity.membersAdded",
+                    Actions = new List<Dialog>
+                    {
+                        new IfCondition()
+                        {
+                            Condition = "$foreach.value.name != turn.activity.recipient.name",
+                            Actions = new List<Dialog>
+                            {
+                                new SendActivity("Welcome to the Dialog Skill Prototype!"),
+                                new BreakLoop(),
+                            }
+                        }
+                    }
+                }
+            };
+
+        private static List<Dialog> AccessASkillSteps { get; } =
+            new List<Dialog>
+            {
+                new SendActivity("Ta da!")
+            };
+    }
+}
